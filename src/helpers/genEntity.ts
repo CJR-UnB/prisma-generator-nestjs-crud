039c96b159e7d1103998a581809f6dcfa9a85cc2
Edson Sales <edsondesouzasales@gmail.com>
2023-01-25 14:54:52 -0300

    refactor/Apaga comentários em genEntity

--- src/helpers/genEntity.ts
+++ src/helpers/genEntity.ts
@@ -3,24 +3,6 @@ import { prismaTypesToTs } from '../utils/prismaTypesToTs'
 import Case from 'case'
 import { changeFirstLetter } from '../utils/changeFirstLetter'
 
-// export function genEntity(model: DMMF.Model) {
-//   const nameUppercase = changeFirstLetter('uppercase', model.name)
-//   const namePascal = Case.pascal(model.name)
-
-//   return (
-// `import { ${nameUppercase} } from "@prisma/client"
-
-// export class ${namePascal}Entity implements ${nameUppercase} {
-// ` +
-//     model.fields
-//       .filter((fild) => fild.kind == 'scalar')
-//       .map((field) => `${field.name}: ${
-//         prismaTypesToTs(field)
-//       }\n`)+
-//     `}`
-// )
-// }
-
 export function genEntity(model: DMMF.Model) {
   const nameUppercase = changeFirstLetter('uppercase', model.name)
   const namePascal = Case.pascal(model.name)
